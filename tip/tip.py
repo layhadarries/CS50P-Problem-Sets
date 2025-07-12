7dd244b0bd3efa4ced845a2a5a485695fea5990a
Layha <layha.darries@gmail.com>
2025-07-12 12:33:36 +0200

    Update tip.py

--- tip/tip.py
+++ tip/tip.py
@@ -8,14 +8,12 @@ def main():
 
 def dollars_to_float(d):
     a = d.strip("$")
-    float_a = float(a) / 100
-    return float_a
+    return float(a) / 100
 
 
 def percent_to_float(p):
     b = p.strip("%")
-    float_b = float(b)
-    return float_b
+    return float(b)
 
 
 main()
