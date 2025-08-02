83810c8ff39bb8aca442f4bb3df281e3b18cf1c8
Layha <layha.darries@gmail.com>
2025-08-02 14:36:28 +0200

    Update figlet.py

--- Week 4/figlet/figlet.py
+++ Week 4/figlet/figlet.py
@@ -1,30 +1,8 @@
 import sys
 from pyfiglet import Figlet
 
-figlet = Figlet()
-
-"""
 def main():
-    # txt_font = input("font: ") # doesn't work lmao
-    text = input("text: ")
-
-    # this is supposed to be "(font=f)" and then when we run the code "python figlet.py -f slant"
-    f = Figlet(font='trashman')
-    print(f.renderText(text))
-    # print(figlet.getFonts()) # gets list of fonts, is a bit slow
-"""
-
-
-def main():
-    try:
-        text = input("Input: ")
-        f = Figlet(font=f)
-        print(f.renderText(text))
-
-    except SystemError:
-        print("Invalid usage")
-        break
-
+    ...
 
 main()
 
