c70d93f0abe659385231e198a1e03e7532c25f15
Layha <layha.darries@gmail.com>
2025-08-05 16:20:19 +0200

    Update fuel.py

--- Week 3/fuel/fuel.py
+++ Week 3/fuel/fuel.py
@@ -10,32 +10,19 @@ def main():
 
 
 def get_percentage():
-
-    #   We want to keep prompting the user for an input.
     while True:
         try:
             text = input("Fraction: ")
-            num, denom = text.split("/")
+            x, y = map(int, text.split("/"))
 
-            # Converts string into int. Need it to check if the input is not an int
-            x = int(num)
-            y = int(denom)
-
-            if y == 0:
-                raise ZeroDivisionError()
-            if y < 0:
-                raise ValueError()
-            if x > y:
-                raise ValueError()
+            if y == 0 or y < 0 or x > y:
+                continue
 
             return round(x / y * 100)  # round() handles floats
 
-        # Error is raised if the input is NOT an int
+        # Error is raised if the input is NOT an int or anything else
         except (ValueError, ZeroDivisionError):
             pass  # test return
 
 
 main()
-
-# check50 cs50/problems/2022/python/fuel
-# submit50 cs50/problems/2022/python/fuel
