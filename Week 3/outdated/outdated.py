a3a49f0dcb11772eebf6681ac69c6c1b2da2fd7b
Layha <layha.darries@gmail.com>
2025-08-05 16:04:52 +0200

    Update outdated.py

--- Week 3/outdated/outdated.py
+++ Week 3/outdated/outdated.py
@@ -15,41 +15,35 @@ def main():
     ]
 
     while True:
-        text = input("Date: ").strip()
+        try:
+            text = input("Date: ").strip()
+    
+            if "/" in text:     # if input is "mm/dd/yyyy"
+                try:
+                    mm, dd, yyyy = map(int, date.split("/"))
+                #   mm, dd, yyyy = text.split("/")     mm = int(mm)   dd = int(dd)
 
-        if "/" in text:     # if input is "mm/dd/yyyy"
-            try:
-                mm, dd, yyyy = text.split("/")
-                mm = int(mm)
-                dd = int(dd)
+                    # Day and Month parameters, condition set to break the loop
+                    if (dd >= 1 and dd <= 31) and (mm >= 1 and mm <= 12):
+                        break   # End the loop so that it only prints once!!
 
-                # Day and Month parameters, condition set to break the loop
-                if (dd >= 1 and dd <= 31) and (mm >= 1 and mm <= 12):
-                    break   # End the loop so that it only prints once!!
+                except ValueError:
+                    continue
 
-            except ValueError:
-                # "when you want to ignore the error and retry (like prompting again for input)" not return, not continue.
-                pass
-
-        elif "," in text:  # If input is "month in months dd, yyyy"
-            try:
-                mm_text, dd, yyyy = text.replace(",", "").split(" ")
-                dd = int(dd)
-
-                if mm_text in months:
-                    mm = months.index(mm_text, 1)
+            elif "," in text:  # If input is "month in months dd, yyyy"
+                try:
+                    mm_text, dd, yyyy = text.replace(",", "").split(" ")
+                    dd = int(dd)
+                    mm = months.index(mm_text, 0) + 1
 
                     # Day parameters, condition set to break the loop
                     if dd >= 1 and dd <= 31:
                         break
 
-            except ValueError:
-                pass
+                except ValueError, IndexError:
+                    continue
 
     print(f'{yyyy}-{mm:02}-{dd:02}')
 
 
 main()
-
-# check50 cs50/problems/2022/python/outdated
-# submit50 cs50/problems/2022/python/outdated
