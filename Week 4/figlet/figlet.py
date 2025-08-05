2842dcc15b718cdfcde43d9239a0c67c41610334
Layha <layha.darries@gmail.com>
2025-08-05 16:34:10 +0200

    Update figlet.py

--- Week 4/figlet/figlet.py
+++ Week 4/figlet/figlet.py
@@ -1,10 +1,33 @@
+# NOTE: For cleaner code, design to check conditions in order:
+#   1-  If it contains "-f" and 3 arguments
+#   2-  if so, check if it contains a valid font
+#   3-  After all conditions are met THEN prompt user for input
+
+#                        sys.argv[0]   sys.argv[1]    sys.argv[2] (font)
+#   Cmd line ->  python   figlet.py        -f            slant
+
+
 import sys
 from pyfiglet import Figlet
 
+
 def main():
-    ...
+    # Must begin with "-f" and contain exatcly 3 arguments
+    if len(sys.argv) != 3 or sys.argv[1] != "-f":
+        sys.exit("Invalid usage")
+
+    font = sys.argv[2]
+    figlet = Figlet()
+
+    # Must contain a valid font
+    if font not in figlet.getFonts():
+        sys.exit("Invalid usage")
+
+    text = input("Input: ")
+    figlet.setFont(font=font)
+
+    print("Output:")
+    print(figlet.renderText(text))
+
 
 main()
-
-#   check50 cs50/problems/2022/python/figlet
-#   submit50 cs50/problems/2022/python/figlet
