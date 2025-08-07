b4666467dc0fb78135747c81f399bc6bbd720a81
layhadarries <layha.darries@gmail.com>
2025-08-07 23:28:32 +0200

    Cleaned up

--- Week 3/outdated/outdated.py
+++ Week 3/outdated/outdated.py
@@ -1,4 +1,4 @@
-ddef main():
+def main():
     months = [
         "January",
         "February",
@@ -17,19 +17,15 @@ ddef main():
     while True:
         try:
             text = input("Date: ").strip()
-<< << << < HEAD
 
             if "/" in text:     # if input is "mm/dd/yyyy"
                 mm, dd, yyyy = map(int, text.split("/"))
                 #   mm, dd, yyyy = text.split("/")     mm = int(mm)   dd = int(dd)
 
-== == == =
-
             if "/" in text:     # if input is "mm/dd/yyyy"
                 mm, dd, yyyy = map(int, date.split("/"))
                 #   mm, dd, yyyy = text.split("/")     mm = int(mm)   dd = int(dd)
 
->>>>>> > 2842dcc15b718cdfcde43d9239a0c67c41610334
             elif "," in text:  # If input is "month in months dd, yyyy"
                 mm_text, dd, yyyy = text.replace(",", "").split(" ")
                 dd = int(dd)
@@ -49,4 +45,3 @@ ddef main():
 
 
 main()
-
