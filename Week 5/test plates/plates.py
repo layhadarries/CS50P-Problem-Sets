2c18ad8d541340a6196542624c0a6c9deca4569c
Layha <layha.darries@gmail.com>
2025-08-12 11:38:58 +0200

    Submitted

--- Week 5/test plates/plates.py
+++ Week 5/test plates/plates.py
@@ -42,35 +42,3 @@ def is_valid(s):
 if __name__ == "__main__":
     main()
 
-
-"""
-def is_valid(plate):
-    # Checks if alphanumeric
-    if plate.isalnum():
-        #Checks if length is allowed
-        if 2 <= len(plate) <= 6:
-            #Checks if first 2 characters are letters
-            if plate[0].isalpha() and plate[1].isalpha():
-                #Finds number finder as a boolean and then
-                number_finder = False
-                for char in plate:
-                    if char.isdigit():
-                        number_finder = True
-                    #If the boolean is true and a letter is found afterwards
-                    elif number_finder and char.isalpha():
-                        return False
-                #Checks to see if first digit is 0
-                for char in plate:
-                    if char.isdigit():
-                        if char == '0':
-                            return False
-                        break        
-                return True 
-            else:
-                return False
-        else:
-            return False     
-    else:
-        return False
-
-"""
